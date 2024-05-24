f724bdec8053f54e2e1bf67502c5e6b63c2cbb32
Wordhater <ryanlocke058@gmail.com>
2024-05-24 16:50:27 +1000

    added settings

--- scripts/core.js
+++ scripts/core.js
@@ -8,3 +8,25 @@ function activatenav(){
             element.classList.add("active"); element.classList.remove("inactive")});
     }
 }
+
+
+function apply_settings(){
+    fetch("../data/settings.json")
+        .then((res) => res.json())
+        .then((json) => {
+            var root = document.querySelector(':root');
+            console.log(json)
+            // set font
+            root.style.setProperty('--font', json.font)
+            // set theme
+            if (json.theme == 1){
+                document.body.classList.add("lightmode")
+            } else {
+                document.body.classList.remove("lightmode")
+            }
+            // WIP animations
+        })
+        .catch((e) => {console.error(e); return 0});
+
+}
+setTimeout(() => {apply_settings()}, 100)
