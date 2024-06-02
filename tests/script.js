6e7d87043b5ac70071e06c04a9664518bc556032
Wordhater <ryanlocke058@gmail.com>
2024-06-02 11:27:52 +1000

    added animations

--- tests/script.js
+++ tests/script.js
@@ -75,3 +75,25 @@ function end_test(test_id) {
     `
     document.getElementById('score').appendChild(create(html))
 }
+
+// Jquery dependant
+
+// scroll animations
+jQuery(function($) {
+    var doAnimations = function() {
+      	var offset = $(window).scrollTop() + $(window).height(),
+        	$animatables = $('.animatable');
+      	if ($animatables.size() == 0) {
+        	$(window).off('scroll', doAnimations);
+      	}
+        $animatables.each(function(i) {
+        	var $animatable = $(this);
+        	if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        	  	$animatable.removeClass('animatable').addClass('animated');
+        	}
+    	});
+    };
+    // Hook doAnimations on scroll, and trigger a scroll
+      $(window).on('scroll', doAnimations);
+    $(window).trigger('scroll');
+});
