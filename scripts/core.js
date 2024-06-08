c35ea296d8a672b0d3ea21ee22417378c7e7a384
Wordhater <ryanlocke058@gmail.com>
2024-06-09 06:33:08 +1000

    cleaned up unused code

--- scripts/core.js
+++ scripts/core.js
@@ -36,15 +36,6 @@ function create(htmlStr) {
     return frag;
 }
 
-// function isInViewport(element) {
-//     const rect = element.getBoundingClientRect();
-//     return (
-//         rect.top >= 0 &&
-//         rect.left >= 0 &&
-//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
-// }
 
 function activatenav(){
     if (document.getElementById("nav-activator").classList.contains("active")){
